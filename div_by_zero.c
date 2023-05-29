bf8b98f3c821a3a193aaec4904a22714092856fc
aarshikjapsen <134580128+aarshikjapsen@users.noreply.github.com>
2023-05-29 15:48:48 +0530

    Create div_by_zero.c

---
+++ div_by_zero.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int numerator = 10;
+    int denominator = 0;
+    int result;
+
+    result = numerator / denominator;
+
+    printf("Result: %d\n", result);
+
+    return 0;
+}
